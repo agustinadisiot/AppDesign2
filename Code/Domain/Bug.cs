eb3b917dda4dcc992c6c56284eef49dbf17c7738
Agustina <agustinadisiotvevo@gmail.com>
2021-09-29 17:18:37 -0300

    Adds missing files from last commit

---
+++ Code/Domain/Bug.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class Bug
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+    }
+}
