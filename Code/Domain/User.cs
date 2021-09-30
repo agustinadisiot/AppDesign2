bf8b63373129dc53d06e7a1566f46b5bab6228fa
Agustina <agustinadisiotvevo@gmail.com>
2021-09-30 17:01:06 -0300

    Adds tests to testUser. Generates class User in Domain. Stage green

---
+++ Code/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain
+{
+    public class User
+    {
+        public User()
+        {
+            Role = null;
+        }
+
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public object Role { get; set; }
+    }
+}
--- Code/TestDomain/TestUser.cs
+++ Code/TestDomain/TestUser.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestDomain
 {
-    class TestUser
+    [TestClass]
+    public class TestUser
     {
+        private User user;
+
+        [TestCleanup]
+        public void TearDown()
+        {
+
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            user = new User()
+            {
+                ID = 0,
+                Username = "agustinadisiot",
+                Name = "Agustina",
+                Lastname = "Disiot",
+                Password = "thisIsNotActuallyMyPassword",
+                Email = "agus@email.com",
+            };
+        }
+
+        [TestMethod]
+        public void IdGetSet()
+        {
+            user.ID = 1;
+            int expected = 1;
+            Assert.AreEqual(expected, user.ID);
+        }
+
+        [TestMethod]
+        public void NameGetSet()
+        {
+            user.Name = "Ivan";
+            string expected = "Ivan";
+            Assert.AreEqual(expected, user.Name);
+        }
+
+        [TestMethod]
+        public void LastnameGetSet()
+        {
+            user.Lastname = "Monjardin";
+            string expected = "Monjardin";
+            Assert.AreEqual(expected, user.Lastname);
+        }
+
+        [TestMethod]
+        public void UsernameGetSet()
+        {
+            user.Username = "ivom";
+            string expected = "ivom";
+            Assert.AreEqual(expected, user.Username);
+        }
+
+        [TestMethod]
+        public void PasswordGetSet()
+        {
+            user.Password = "myPasscode";
+            string expected = "myPasscode";
+            Assert.AreEqual(expected, user.Password);
+        }
+
+        [TestMethod]
+        public void NullRole()
+        {
+            Assert.IsNull(user.Role);
+        }
+
     }
 }
