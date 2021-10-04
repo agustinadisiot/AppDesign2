a87f96081832b745de7e9c7b7e1a6e4cd32964e7
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-03 23:31:48 -0300

    Adds new bug design migraiton

--- Code/Repository/Migrations/20211004023001_ModifiesBugProjectRelationship.Designer.cs
+++ Code/Repository/Migrations/20211004023001_ModifiesBugProjectRelationship.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
 namespace Repository.Migrations
 {
     [DbContext(typeof(BugManagerContext))]
-    partial class BugManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20211004023001_ModifiesBugProjectRelationship")]
+    partial class ModifiesBugProjectRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
