c351527464f8b5c65e064ac66a60c9be7a1a1a2b
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-05 23:38:13 -0300

    Adds create admin test data access. Stage red

---
+++ Code/TestDataAccess/TestAdminDataAccess.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Domain;
+using Domain.Utils;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+
+namespace TestDataAccess
+{
+    [TestClass]
+    public class TestAdminDataAccess
+    {
+
+        private readonly DbConnection connection;
+        private readonly AdminDataAccess adminDataAccess;
+        private readonly BugManagerContext bugManagerContext;
+        private readonly DbContextOptions<BugManagerContext> contextOptions;
+
+        public TestAdminDataAccess()
+        {
+            connection = new SqliteConnection("Filename=:memory:");
+            contextOptions = new DbContextOptionsBuilder<BugManagerContext>().UseSqlite(connection).Options;
+            bugManagerContext = new BugManagerContext(contextOptions);
+            adminDataAccess = new adminDataAccess(bugManagerContext);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            connection.Open();
+            bugManagerContext.Database.EnsureCreated();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            bugManagerContext.Database.EnsureDeleted();
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            Admin expectedAdmin = new Admin
+            {
+                Id = 1,
+                Username = "administradorPedro",
+                Name = "Pedro",
+                Lastname = "López",
+                Password = "fransico234",
+                Email = "pedrooo2@hotmail.com"
+
+            };
+
+            adminDataAccess.Create(new Admin()
+            {
+                Id = 1,
+                Username = "administradorPedro",
+                Name = "Pedro",
+                Lastname = "López",
+                Password = "fransico234",
+                Email = "pedrooo2@hotmail.com"
+            });
+
+            var adminSaved = adminDataAccess.GetById(1);
+            Assert.AreEqual(0, new AdminComparer().Compare(expectedAdmin, adminSaved));
+
+        }
+    }
+}
