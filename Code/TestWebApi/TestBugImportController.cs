cc0cd63d060f6b40d551b92ae67931842810ad22
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-06 10:34:52 -0300

    Adds tests for bug import web api. Stage red

---
+++ Code/TestWebApi/TestBugImportController.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using BusinessLogic;
+using BusinessLogicInterfaces;
+using Domain;
+using Domain.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebApi.Controllers;
+
+
+namespace TestWebApi
+{
+    [TestClass]
+    public class TestBugImportController
+    {
+
+
+        [TestMethod]
+        public void ImportXML()
+        {
+            List<Bug> bugsExpected = new List<Bug>()
+            {
+                new Bug(){
+                Name = "Not working button",
+                Description = "Upload button not working",
+                Version = "1",
+                IsActive = true,
+                CompletedBy = null,
+                Id = 0
+                },
+                new Bug(){
+                Name = "Not working button",
+                Description = "Upload button not working",
+                Version = "1",
+                IsActive = true,
+                CompletedBy = null,
+                Id = 1
+                }
+            };
+
+            string path = "file.xml";
+            var mock = new Mock<IBugBusinessLogic>(MockBehavior.Strict);
+            mock.Setup(b => b.ImportXML(path, IMPORT_COMPANY.XML)).Returns(bugsExpected);
+            var controller = new BugController(mock.Object);
+
+            var result = controller.Get();
+            var okResult = result as OkObjectResult;
+            var bugsResult = okResult.Value as IEnumerable<Bug>;
+
+            mock.VerifyAll();
+            CollectionAssert.AreEqual(bugsExpected, (System.Collections.ICollection)bugsResult, new BugComparer());
+        }
+
+    }
+}
+
