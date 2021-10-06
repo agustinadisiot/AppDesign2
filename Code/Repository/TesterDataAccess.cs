7492f85c63614afd61407d9bbc644b396f64348a
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-06 03:13:49 -0300

    Adds tests for tester creation data access

---
+++ Code/Repository/TesterDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogic;
+using BusinessLogicInterfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Repository.Design;
+using RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class TesterDataAccess : ITesterDataAccess
+    {
+        private readonly DbSet<Tester> testers;
+        private readonly BugManagerContext context;
+
+        public TesterDataAccess(DbContext newContext)
+        {
+            context = (BugManagerContext)newContext;
+            testers = context.Set<Tester>();
+        }
+
+        public Tester Create(Tester tester)
+        {
+
+            if (tester is null)
+            {
+                // TODO
+            }
+
+            context.Add(tester);
+            context.SaveChanges();
+
+            return tester;
+        }
+    }
+}
---
+++ Code/TestDataAccess/TestTesterDataAccess.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Domain;
+using Domain.Utils;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+
+namespace TestDataAccess
+{
+    [TestClass]
+    public class TestTesterDataAccess
+    {
+
+        private readonly DbConnection connection;
+        private readonly TesterDataAccess testerDataAccess;
+        private readonly BugManagerContext bugManagerContext;
+        private readonly DbContextOptions<BugManagerContext> contextOptions;
+
+        public TestTesterDataAccess()
+        {
+            connection = new SqliteConnection("Filename=:memory:");
+            contextOptions = new DbContextOptionsBuilder<BugManagerContext>().UseSqlite(connection).Options;
+            bugManagerContext = new BugManagerContext(contextOptions);
+            testerDataAccess = new TesterDataAccess(bugManagerContext);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            connection.Open();
+            bugManagerContext.Database.EnsureCreated();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            bugManagerContext.Database.EnsureDeleted();
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            Developer expectedDev = new Developer
+            {
+                Username = "developerPedro",
+                Name = "Pedro",
+                Lastname = "López",
+                Password = "fransico234",
+                Email = "pedrooo2@hotmail.com"
+
+            };
+
+            Tester devSaved = testerDataAccess.Create(new Tester()
+            {
+                Username = "testerPedro",
+                Name = "Pedro",
+                Lastname = "López",
+                Password = "fransico234",
+                Email = "pedrooo2@hotmail.com"
+            });
+
+            Assert.AreEqual(0, new UserComparer().Compare(expectedDev, devSaved));
+
+        }
+    }
+}
