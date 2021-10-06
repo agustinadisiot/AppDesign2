2fb2310e60b6550530e65fe81780b75b61aaa8cc
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-06 02:28:35 -0300

    Adds developer create tests business logic. Stage green

---
+++ Code/RepositoryInterfaces/IDeveloperDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLogicInterfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+
+namespace RepositoryInterfaces
+{
+    public interface IDeveloperDataAccess
+    {
+        public Developer Create(Developer dev);
+    }
+}
