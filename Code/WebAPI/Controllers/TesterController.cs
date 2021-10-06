30c9bb486676062a107f9c1c87d3fad2b1f06239
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-06 02:49:09 -0300

    Adds tests for tester creation web api. Stage green

---
+++ Code/WebAPI/Controllers/TesterController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogicInterfaces;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("testers")]
+    public class TesterController : ControllerBase
+    {
+        private readonly ITesterBusinessLogic businessLogic;
+
+        public TesterController(ITesterBusinessLogic newTesterBusinessLogic)
+        {
+            businessLogic = newTesterBusinessLogic;
+        }
+
+        [HttpPost]
+        public object Post([FromBody] Tester testerExpected)
+        {
+            return Ok(businessLogic.Add(testerExpected));
+        }
+
+    }
+}
