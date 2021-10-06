33f631502c4cdbc5773da964cb36429c88a4be79
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-05 23:32:53 -0300

    Adds create admin test business logic. Stage green

---
+++ Code/BusinessLogic/AdminBusinessLogic.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using System;
+using BusinessLogicInterfaces;
+using System.Collections.Generic;
+using System.Linq;
+using RepositoryInterfaces;
+
+namespace BusinessLogic
+{
+    public class AdminBusinessLogic : IAdminBusinessLogic
+    {
+        public IAdminDataAccess adminDataAccess { get; set; }
+
+        public AdminBusinessLogic(IAdminDataAccess newAdminDataAccess)
+        {
+            adminDataAccess = newAdminDataAccess;
+        }
+
+        public Admin Add(Admin admin)
+        {
+            adminDataAccess.Create(admin);
+            return admin;
+        }
+
+    }
+
+
+}
