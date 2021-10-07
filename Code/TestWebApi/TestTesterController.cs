2fb42a9bb1403123ba3bf4e0cebb639792b26aff
Agustina <agustinadisiotvevo@gmail.com>
2021-10-07 01:48:25 -0300

    Adds test to filter by bug to tester controller. Stage red

--- Code/TestWebApi/TestTesterController.cs
+++ Code/TestWebApi/TestTesterController.cs
@@ -40,6 +40,35 @@ namespace TestWebApi
             mock.VerifyAll();
             Assert.AreEqual(testerExpected, devResult);
         }
+
+        [TestMethod]
+        public void FilterBugsByStatus()
+        {
+            List<Bug> bugsExpected = new List<Bug>()
+            {
+                new Bug()
+                {
+                    Name = "bug1",
+                    IsActive = true
+                },
+                new Bug()
+                {
+                    Name = "bug2",
+                    IsActive = true
+                },
+            };
+            int idTester = 1;
+            var mock = new Mock<ITesterBusinessLogic>(MockBehavior.Strict);
+            mock.Setup(t => t.GetBugsByStatus(idTester,true)).Returns(bugsExpected);
+            var controller = new TesterController(mock.Object);
+
+            var result = controller.GetBugsByStatus(idTester, true);
+            var okResult = result as OkObjectResult;
+            var bugsResult = okResult.Value as List<Bug>;
+
+            mock.VerifyAll();
+            Assert.AreEqual(bugsExpected, bugsResult);
+        }
     }
 };
 
