4276277ddcd416ab83163b632a4466900a817a89
Agustina <agustinadisiotvevo@gmail.com>
2021-10-19 18:33:09 -0300

    Adds get and set tests Work to Domain. Stage green

---
+++ Code/Domain/Work.cs
@@ -0,0 +1,9 @@
+﻿namespace TestDomain
+{
+    public class Work
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public int Time { get; set; }
+    }
+}
--- Code/TestDomain/TestWork.cs
+++ Code/TestDomain/TestWork.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TestDomain
 {
@@ -21,67 +21,31 @@ namespace TestDomain
         {
             work = new Work()
             {
-                Name = "Project",
+                Name = "Work",
             };
         }
         [TestMethod]
         public void NameGetSet()
         {
-            work.Name = "Project1";
-            string expected = "Project1";
-            Assert.AreEqual(expected, project.Name);
+            work.Name = "work1";
+            string expected = "work1";
+            Assert.AreEqual(expected, work.Name);
         }
 
         [TestMethod]
-        public void GetTestersEmpty()
+        public void CostGetSet()
         {
-            List<Tester> expectedTesters = new List<Tester>();
-            Assert.IsTrue(expectedTesters.SequenceEqual(project.Testers));
+            work.Cost = 4;
+            int expected = 4;
+            Assert.AreEqual(expected, work.Cost);
         }
 
         [TestMethod]
-        public void GetSetTesters()
+        public void TimeGetSet()
         {
-            Tester tester = new Tester();
-            List<Tester> expectedTesters = new List<Tester>();
-            expectedTesters.Add(tester);
-            project.Testers = expectedTesters;
-            Assert.IsTrue(expectedTesters.SequenceEqual(project.Testers));
+            work.Time = 3;
+            int expected = 3;
+            Assert.AreEqual(expected, work.Time);
         }
-
-        [TestMethod]
-        public void GetDevelopersEmpty()
-        {
-            List<Developer> expectedDeveloper = new List<Developer>();
-            Assert.IsTrue(expectedDeveloper.SequenceEqual(project.Developers));
-        }
-
-        [TestMethod]
-        public void GetSetDevelopers()
-        {
-            Developer dev = new Developer();
-            List<Developer> expectedDeveloper = new List<Developer>();
-            expectedDeveloper.Add(dev);
-            project.Developers = expectedDeveloper;
-            Assert.IsTrue(expectedDeveloper.SequenceEqual(project.Developers));
-        }
-
-        [TestMethod]
-        public void GetBugsEmpty()
-        {
-            List<Bug> expectedBugs = new List<Bug>();
-            Assert.IsTrue(expectedBugs.SequenceEqual(project.Bugs));
-        }
-
-        [TestMethod]
-        public void GetSetBugs()
-        {
-            Bug bug = new Bug();
-            List<Bug> expectedBugs = new List<Bug>();
-            expectedBugs.Add(bug);
-            project.Bugs = expectedBugs;
-            Assert.IsTrue(expectedBugs.SequenceEqual(project.Bugs));
-        }
-
     }
 }
