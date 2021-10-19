f1af642fddb7127b56bf16997f7459c586b3fa3d
Agustina <agustinadisiotvevo@gmail.com>
2021-10-19 19:18:06 -0300

    Adds property Cost to devs and testers. Stage green

--- Code/Domain/Developer.cs
+++ Code/Domain/Developer.cs
@@ -5,6 +5,8 @@ namespace Domain
     public class Developer : User
     {
         public List<Project> Projects { get; set; }
+        public int Cost { get; set; }
+
         public Developer()
         {
             Projects = new List<Project>();
