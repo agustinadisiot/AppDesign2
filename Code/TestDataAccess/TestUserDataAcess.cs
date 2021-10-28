55c8d8a6c781f43ec834a9df6f42523c442dfac1
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-27 21:11:35 -0300

    Refactor to add userDataAcessTest. Stage red

---
+++ Code/TestDataAccess/TestUserDataAcess.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Domain;
+using Domain.Utils;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+
+namespace TestDataAccess
+{
+    [TestClass]
+    public abstract class TestUserDataAccess
+    {
+        protected abstract IUserDataAcess GetUserDataAccess;
+        private readonly DbConnection connection;
+        private readonly LoginDataAccess loginDataAccess;
+        private readonly BugManagerContext bugManagerContext;
+        private readonly DbContextOptions<BugManagerContext> contextOptions;
+
+        public TestUserDataAccess()
+        {
+            connection = new SqliteConnection("Filename=:memory:");
+            contextOptions = new DbContextOptionsBuilder<BugManagerContext>().UseSqlite(connection).Options;
+            bugManagerContext = new BugManagerContext(contextOptions);
+            loginDataAccess = new LoginDataAccess(bugManagerContext);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            connection.Open();
+            bugManagerContext.Database.EnsureCreated();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            bugManagerContext.Database.EnsureDeleted();
+        }
+    }
+}
