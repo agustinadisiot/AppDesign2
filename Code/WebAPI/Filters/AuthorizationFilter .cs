266de085abd6fdd17df3c25215a4c6d0d48d0391
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-10-30 20:38:50 -0300

    Refactors authorization filter

--- Code/WebAPI/Filters/AuthorizationFilter .cs
+++ Code/WebAPI/Filters/AuthorizationFilter .cs
@@ -29,40 +29,44 @@ namespace WebApi.Filters
             string token = context.HttpContext.Request.Headers["token"];
             if (token == null)
             {
-                ResponseMessage message = new ResponseMessage("You aren't logued.");
+                NotLoguedRespond(context);
+                return;
+            }
+
+            if (arg.Contains("Admin"))
+            {
+                var logic = context.HttpContext.RequestServices.GetService<IAdminBusinessLogic>();
+                isAuthorize = isAuthorize || logic.VerifyRole(token);
+            }
+            if (arg.Contains("Developer"))
+            {
+                var logic = context.HttpContext.RequestServices.GetService<IDeveloperBusinessLogic>();
+                isAuthorize = isAuthorize || logic.VerifyRole(token);
+            }
+            if (arg.Contains("Tester"))
+            {
+                var logic = context.HttpContext.RequestServices.GetService<ITesterBusinessLogic>();
+                isAuthorize = isAuthorize || logic.VerifyRole(token);
+            }
+
+            if (!isAuthorize)
+            {
+                ResponseMessage message = new ResponseMessage("You aren't logued correctly.");
                 context.Result = new ObjectResult(message)
                 {
-                    StatusCode = 401,
+                    StatusCode = 403,
                 };
             }
-            else
+
+        }
+
+        private void NotLoguedRespond(AuthorizationFilterContext context)
+        {
+            ResponseMessage message = new ResponseMessage("You aren't logued.");
+            context.Result = new ObjectResult(message)
             {
-                if (arg.Contains("Admin"))
-                {
-                    var logic = context.HttpContext.RequestServices.GetService<IAdminBusinessLogic>();
-                    isAuthorize = isAuthorize || logic.VerifyRole(token);
-                }
-                if (arg.Contains("Developer"))
-                {
-                    var logic = context.HttpContext.RequestServices.GetService<IDeveloperBusinessLogic>();
-                    isAuthorize = isAuthorize || logic.VerifyRole(token);
-                }
-                if (arg.Contains("Tester"))
-                {
-                    var logic = context.HttpContext.RequestServices.GetService<ITesterBusinessLogic>();
-                    isAuthorize = isAuthorize || logic.VerifyRole(token);
-                }
-                if (!isAuthorize)
-                {
-                    ResponseMessage message = new ResponseMessage("You aren't logued correctly.");
-                    context.Result = new ObjectResult(message)
-                    {
-                        StatusCode = 403,
-                    };
-                }
-
-            }
-
+                StatusCode = 401,
+            };
         }
     }
 };
