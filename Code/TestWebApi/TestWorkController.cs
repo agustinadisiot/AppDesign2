185f6da3ad84733041cb1fd345e19a59b27ff050
Agustina <agustinadisiotvevo@gmail.com>
2021-10-31 18:20:30 -0300

    Adds web api test to work controller. Stage red

---
+++ Code/TestWebApi/TestWorkController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestWebApi
+{
+    [TestClass]
+    public class TestWorkController
+    {
+        [TestMethod]
+        public void Create()
+        {
+            Work workExpected = new Work()
+            {
+                Name = "Not working button",
+                Cost = 2,
+                Time = 4,
+                Id = 3
+            };
+
+            var mock = new Mock<IWorkBusinessLogic>(MockBehavior.Strict);
+            mock.Setup(b => b.Add(workExpected)).Returns(workExpected);
+            var controller = new WorkController(mock.Object);
+
+            var result = controller.Post(workExpected);
+            var okResult = result as OkObjectResult;
+            var workResult = okResult.Value as Work;
+
+            mock.VerifyAll();
+            Assert.AreEqual(workExpected, workResult);
+        }
+
+        [TestMethod]
+        public void GetWork()
+        {
+            Work workExpected = new Work()
+            {
+                Name = "Not working button",
+                Cost = 2,
+                Time = 4,
+                Id = 3
+            };
+
+            var mock = new Mock<IWorkBusinessLogic>(MockBehavior.Strict);
+            mock.Setup(b => b.GetById(workExpected.Id)).Returns(workExpected);
+            var controller = new WorkController(mock.Object);
+
+            var result = controller.Get(workExpected.Id);
+            var okResult = result as OkObjectResult;
+            var workResult = okResult.Value as Work;
+
+            mock.VerifyAll();
+            Assert.AreEqual(workExpected, workResult);
+        }
+    }
+}
