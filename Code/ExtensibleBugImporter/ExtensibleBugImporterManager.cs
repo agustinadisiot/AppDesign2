cb7a0273919f3e69e61563820cc5077cba5a92db
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-11-04 01:16:13 -0300

    Adds test for two importers. Stage green

--- Code/ExtensibleBugImporter/ExtensibleBugImporterManager.cs
+++ Code/ExtensibleBugImporter/ExtensibleBugImporterManager.cs
@@ -14,23 +14,28 @@ namespace ExtensibleBugImporter
         public List<ImporterInfo> GetAvailableImporters(string path = defaultPath)
         {
             List<ImporterInfo> importerInfos = new List<ImporterInfo>();
-            FileInfo dllFile = new FileInfo(path);
-            Assembly assembly = Assembly.LoadFile(dllFile.FullName);
-            foreach (Type type in assembly.GetTypes())
-            {
-                try
-                {
-                    if (typeof(IBugImporter).IsAssignableFrom(type))
-                    {
-                        IBugImporter provider = (IBugImporter)Activator.CreateInstance(type);
-                        importerInfos.Add(provider.GetImporterInfo());
-                    }
 
-                }
-                catch (Exception e)
+            string[] filePaths = Directory.GetFiles(path);
+            foreach (string filePath in filePaths)
+            {
+                FileInfo dllFile = new FileInfo(filePath);
+                Assembly assembly = Assembly.LoadFile(dllFile.FullName);
+                foreach (Type type in assembly.GetTypes())
                 {
-                    // TODO revise error handling
-                    // example: when a importer doesn't compile?
+                    try
+                    {
+                        if (typeof(IBugImporter).IsAssignableFrom(type))
+                        {
+                            IBugImporter provider = (IBugImporter)Activator.CreateInstance(type);
+                            importerInfos.Add(provider.GetImporterInfo());
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        // TODO revise error handling
+                        // example: when a importer doesn't compile?
+                    }
                 }
             }
             return importerInfos;
