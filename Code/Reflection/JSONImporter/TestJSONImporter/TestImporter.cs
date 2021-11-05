b57192d62d27e21a9b7b71cd6e086864c7497083
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-11-05 00:34:24 -0300

    Adds no file json import test. Stage red

--- Code/Reflection/JSONImporter/TestJSONImporter/TestImporter.cs
+++ Code/Reflection/JSONImporter/TestJSONImporter/TestImporter.cs
@@ -159,5 +159,23 @@ namespace TestJSONImporter
             );
 
         }
+
+        [TestMethod]
+        public void NoFileImport()
+        {
+            string fullPath = baseDirectory + "adsfasdf";
+            List<Parameter> parameters = new List<Parameter>()
+            {
+                new Parameter(){
+                    Name = "path",
+                    Type = ParameterType.STRING,
+                    Value = fullPath
+                }
+            };
+            Assert.ThrowsException<CustomImporterException>(() =>
+                            jsonImporter.ImportBugs(parameters)
+            );
+
+        }
     }
 }
