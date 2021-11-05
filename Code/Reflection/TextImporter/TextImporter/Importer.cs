1bc426ed86f40c87bdfcd3d1a91cdfb61de2c261
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-11-05 01:25:42 -0300

    Adds get info positional text importer. Stage green

--- Code/Reflection/TextImporter/TextImporter/Importer.cs
+++ Code/Reflection/TextImporter/TextImporter/Importer.cs
@@ -6,9 +6,28 @@ namespace TextImporter
 {
     public class Importer : IBugImporter
     {
+        private ImporterInfo info;
+        public Importer()
+        {
+            info = new ImporterInfo()
+            {
+                ImporterName = "Positional Text Importer",
+                Params = new List<Parameter>
+                {
+                    new Parameter(){
+                    Name = "Folder path",
+                    Type = ParameterType.STRING,
+                },
+                new Parameter(){
+                    Name = "File Name",
+                    Type = ParameterType.INTEGER,
+                }
+                }
+            };
+        }
         public ImporterInfo GetImporterInfo()
         {
-            throw new NotImplementedException();
+            return info;
         }
 
         public List<ImportedBug> ImportBugs(List<Parameter> parameters)
