e2fce6b82d74ed9c599d2579655690fa0ead2218
Iv�n Monjardin <IM239850@fi365.ort.edu.uy>
2021-11-07 20:20:04 -0300

    Changes routes

--- Frontend/ATemplate-argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/ATemplate-argon-dashboard-angular-master/src/app/components/sidebar/sidebar.component.ts
@@ -7,14 +7,14 @@ declare interface RouteInfo {
     icon: string;
     class: string;
 }
-export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Logout',  icon:'ni-key-25 text-info', class: '' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+export const ROUTES: RouteInfo[] = [ // EXP: Aca es donde van los links de la sidebar  
+    { path: '/admin/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
+    { path: '/admin/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
+    { path: '/admin/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
+    { path: '/admin/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
+    { path: '/admin/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: 'login', title: 'Logout',  icon:'ni-key-25 text-info', class: '' },
+    { path: 'register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
 ];
 
 @Component({
