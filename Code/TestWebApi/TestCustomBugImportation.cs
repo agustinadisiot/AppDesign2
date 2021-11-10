382cd30b1b741d6c90922bd473b892f1d2471344
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-11-10 01:14:21 -0300

    Adds test for web api get custom importers. Stage red

---
+++ Code/TestWebApi/TestCustomBugImportation.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLogicInterfaces;
+using CustomBugImportation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebApi
+{
+    [TestClass]
+    public class TestCustomBugImportation
+    {
+        [TestMethod]
+        public void GetImporters()
+        {
+
+            List<ImporterInfo> expectedImportersInfo = new List<ImporterInfo>
+            {
+                new ImporterInfo {
+                                ImporterName = "Empty Importer",
+                                Params = new List<Parameter>{}
+                },
+                new ImporterInfo {
+                                ImporterName = "Empty Importer 2",
+                                Params = new List<Parameter>{}
+                }
+            };
+
+            var mock = new Mock<IBugBusinessLogic>(MockBehavior.Strict);
+            mock.Setup(b => b.GetCustomImportersInfo());
+            var controller = new BugController(mock.Object);
+
+            List<ImporterInfo> actualImportersInfo = controller.GetCustomImportersInfo();
+
+            mock.VerifyAll();
+            CollectionAssert.AreEquivalent(expectedImportersInfo, actualImportersInfo);
+        }
+    }
+}
