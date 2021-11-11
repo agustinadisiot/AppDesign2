862236cb99af5c87abf9abfa49b040cef96f7cf1
Agustina <agustinadisiotvevo@gmail.com>
2021-11-11 20:56:19 -0300

    Adds tests to resolve and unresolve bug web api. Stage red

--- Backend/TestWebApi/TestBugController.cs
+++ Backend/TestWebApi/TestBugController.cs
@@ -149,6 +149,56 @@ namespace TestWebApi
             mock.VerifyAll();
             Assert.AreEqual(bugExpected, bugResult);
         }
+
+        [TestMethod]
+        public void ResolveBug()
+        {
+            BugDTO bugExpected = new BugDTO()
+            {
+                Name = "Not working button",
+                Description = "Upload button not working",
+                Version = "1",
+                IsActive = true,
+                CompletedById = 0,
+                Id = 0
+            };
+
+            var mock = new Mock<IBugBusinessLogic>(MockBehavior.Strict);
+            mock.Setup(b => b.ResolveBug(bugExpected.Id)).Returns(bugExpected);
+            var controller = new BugController(mock.Object);
+
+            var result = controller.ResolveBug(bugExpected.Id);
+            var okResult = result as OkObjectResult;
+            var bugResult = okResult.Value as BugDTO;
+
+            mock.VerifyAll();
+            Assert.AreEqual(bugExpected, bugResult);
+        }
+
+        [TestMethod]
+        public void UnresolveBug()
+        {
+            BugDTO bugExpected = new BugDTO()
+            {
+                Name = "Not working button",
+                Description = "Upload button not working",
+                Version = "1",
+                IsActive = false,
+                CompletedById = 4,
+                Id = 0
+            };
+
+            var mock = new Mock<IBugBusinessLogic>(MockBehavior.Strict);
+            mock.Setup(b => b.ResolveBug(bugExpected.Id)).Returns(bugExpected);
+            var controller = new BugController(mock.Object);
+
+            var result = controller.ResolveBug(bugExpected.Id);
+            var okResult = result as OkObjectResult;
+            var bugResult = okResult.Value as BugDTO;
+
+            mock.VerifyAll();
+            Assert.AreEqual(bugExpected, bugResult);
+        }
     }
 }
 
