fba108d73d6b95000a24ac84d41a6e2cd11ac280
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-11-11 00:45:43 -0300

    Adds api uri to environment and removes env replacement

--- Frontend/BugManager/src/app/services/login/login.service.ts
+++ Frontend/BugManager/src/app/services/login/login.service.ts
@@ -2,16 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from 'src/app/models/loginResponse';
 import { UserCredentials } from 'src/app/models/userCredentials';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  endpoint = `${environment.webApi_origin}/login`;
 
   constructor(private http: HttpClient) { }
 
   login(credentials: UserCredentials) {
-    this.http.get<LoginResponse>("https://localhost:5002/login").subscribe(
+    this.http.get<LoginResponse>(this.endpoint).subscribe(
       response => {
         return response;
       },
